1be75bc72978b39679084f64cd6edc857a17df33
ammar <ammarraneez@gmail.com>
2022-01-17 21:26:22 +0530

    feat: add number class and node to keep track of value

--- node.py
+++ node.py
@@ -1,6 +1,20 @@
+### Number Node - Just an integer or float ###
+class NumberNode:
+    # tok -> a number Token
+    # pos_start -> the node itself
+    # pos_end -> also the node itself
+    def __init__(self, tok):
+        self.tok = tok
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
+    def __repr__(self):
+        return f'{self.tok}'
+
+
 # Will hold saved variable names
 class VarAccessNode:
-    # var_name_token -> variable name
+    # var_name_tok -> variable name
     # pos_start -> start of variable name
     # pos_end -> the end of variable name itself
     def __init__(self, var_name_tok):
@@ -11,7 +25,7 @@ class VarAccessNode:
 
 # will hold variable assignment
 class VarAssignNode:
-    # var_name_token -> variable name
+    # var_name_tok -> variable name
     # value_node -> variables value
     # pos_start -> start of variable name
     # pos_end -> the value nodes end position
---
+++ number.py
@@ -0,0 +1,21 @@
+# keep track of the result
+class Number:
+    # pos_start -> Position of the start of the number
+    # pos_end -> Position of the end of the number
+    # value -> the actual value of the number
+    def __init__(self, value):
+        self.context = None
+        self.pos_end = None
+        self.pos_start = None
+        self.value = value
+        self.set_pos()
+        self.set_context()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def set_context(self, context=None):
+        self.context = context
+        return self
