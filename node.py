8d381b326a82336a5a415d00ba92da666bb77d4a
ammar <ammarraneez@gmail.com>
2022-01-18 19:19:20 +0530

    feat: enhance nodes to contain binary operations or unary operations

--- node.py
+++ node.py
@@ -12,6 +12,40 @@ class NumberNode:
         return f'{self.tok}'
 
 
+### Binary Operation Node - Add/Multiply/Subtract/Divide of two nodes ###
+class BinOpNode:
+    # left_node -> a number Token
+    # op_tok -> an operator Token
+    # right_node -> a number Token
+    # pos_start -> the left nodes start
+    # pos_end -> the right nodes end
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+
+### Unary Operation Node - (Ex: -5) ###
+class UnaryOpNode:
+    # op_token -> an operator Token
+    # node -> a number Token
+    # pos_start -> start (will be the operator (-5)
+    # pos_end -> the node itself
+    def __init__(self, op_tok, node):
+        self.op_tok = op_tok
+        self.node = node
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
+
+    def __repr__(self):
+        return f'({self.op_tok}, {self.node})'
+
+
 # Will hold saved variable names
 class VarAccessNode:
     # var_name_tok -> variable name
