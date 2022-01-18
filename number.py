11730e3fa5e02f5a235ba41d49c61525ffd187c9
ammar <ammarraneez@gmail.com>
2022-01-18 19:35:32 +0530

    feat: enhance number to be able to have math operations

--- number.py
+++ number.py
@@ -1,3 +1,6 @@
+from error import RTError
+
+
 # keep track of the result
 class Number:
     # pos_start -> Position of the start of the number
@@ -19,6 +22,38 @@ class Number:
     def set_context(self, context=None):
         self.context = context
         return self
+
+    # add a number - cannot have errors
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value).set_context(self.context), None
+
+    # subtract by a number - cannot have errors
+    def subbed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value).set_context(self.context), None
+
+    # multiplied by a number - cannot have errors
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value).set_context(self.context), None
+
+    # divided by a number - possible to have divided by 0 error
+    def dived_by(self, other):
+        if isinstance(other, Number):
+            # disallow divisions by zero
+            if other.value == 0:
+                return None, RTError(
+                    other.pos_start, other.pos_end,
+                    'Division by zero',
+                    self.context
+                )
+
+            return Number(self.value / other.value).set_context(self.context), None
+
+    def powed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value ** other.value).set_context(self.context), None
 
     def copy(self):
         copy = Number(self.value)
