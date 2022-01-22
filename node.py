06e06e28be3b824c2f19df5cb13622868edc2e6a
ammar <ammarraneez@gmail.com>
2022-01-22 21:43:25 +0530

    feat: update nodes with while and for node

--- node.py
+++ node.py
@@ -79,3 +79,33 @@ class IfNode:
         self.else_case = else_case
         self.pos_start = self.cases[0][0].pos_start
         self.pos_end = (self.else_case or self.cases[len(self.cases) - 1][0]).pos_end
+
+
+# For loop node
+class ForNode:
+    # var_name_tok -> variable name
+    # start_value_node -> start of loop
+    # end_value_node -> end of loop
+    # step_value_node -> step counter
+    # body_node -> body of loop
+    def __init__(self, var_name_tok, start_value_node, end_value_node, step_value_node, body_node):
+        self.var_name_tok = var_name_tok
+        self.start_value_node = start_value_node
+        self.end_value_node = end_value_node
+        self.step_value_node = step_value_node
+        self.body_node = body_node
+
+        self.pos_start = self.var_name_tok.pos_start
+        self.pos_end = self.body_node.pos_end
+
+
+# While loop node
+class WhileNode:
+    # condition_node -> while loop condition
+    # body_node -> body of loop
+    def __init__(self, condition_node, body_node):
+        self.condition_node = condition_node
+        self.body_node = body_node
+
+        self.pos_start = self.condition_node.pos_start
+        self.pos_end = self.body_node.pos_end
